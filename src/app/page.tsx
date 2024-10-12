4a309e13e6093eb0d6560cd7a922b34bf72638c3
vishnu5104 <vishnus510412@gmail.com>
2024-10-13 01:04:56 +0530

    Token For Investor Creator so any Investor can create there own token then fund the game builder with that token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import CreatePage from '@/components/Home';
+import SmartContractUI from '@/components/MintNFt';
 
 export default function Home() {
   return (
     <div>
       ZentyLabs
-      <CreatePage />
+      {/* <CreatePage /> */}
+      <SmartContractUI />
     </div>
   );
 }
