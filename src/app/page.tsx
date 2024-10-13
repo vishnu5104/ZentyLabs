7d999899b2c929fc54456ff6b8caad9074a94960
vishnu5104 <vishnus510412@gmail.com>
2024-10-13 12:40:51 +0530

    NFT page implemened

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import CreatePage from '@/components/Home';
 import SmartContractUI from '@/components/MintNFt';
+import NFTMarketplaceUI from '@/components/NFT';
 
 export default function Home() {
   return (
     <div>
       ZentyLabs
       {/* <CreatePage /> */}
-      <SmartContractUI />
+      {/* <SmartContractUI /> */}
+      <NFTMarketplaceUI />
     </div>
   );
 }
